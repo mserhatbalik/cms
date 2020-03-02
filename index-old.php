8d8b1f895bdbe994beeaf632aec0e42430160035
Serhat Balık <mserhatbalik@gmail.com>
2020-03-02 14:37:24 +0300

    Implemented pagination system for blog posts

--- index-old.php
+++ index-old.php
@@ -15,30 +15,7 @@
             ini_set('display_errors', 1);
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
-
-            $per_page = 3;
-
-            if (isset($_GET['page'])) {
-
-                $page = $_GET['page'];
-            } else {
-                $page = "";
-            }
-
-            if ($page == "" || $page == 1) {
-                $page_1 = 0;
-            } else {
-                $page_1 = ($page * $per_page) - $per_page;
-            }
-
-
-            $post_query_count = "SELECT * FROM posts";
-            $find_count = mysqli_query($connection, $post_query_count);
-            $count = mysqli_num_rows($find_count);
-            $count = ceil($count / $per_page);
-
-
-            $query = "SELECT * FROM posts LIMIT $page_1, $per_page";
+            $query = "SELECT * FROM posts";
             $selectAllPostsQuery = mysqli_query($connection, $query);
             while ($row = mysqli_fetch_assoc($selectAllPostsQuery)) {
                 $post_id = $row['post_id'];
@@ -61,7 +38,6 @@
                 </h1> -->
 
                     <!-- First Blog Post -->
-
                     <h2>
                         <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                     </h2>
@@ -97,41 +73,10 @@
         <?php include "./includes/sidebar.php"; ?>
 
 
+
     </div>
     <!-- /.row -->
 
     <hr>
-
-
-    <ul class="pager">
-        <?php
-        if (!isset($_GET['page'])) {
-            $pager_one = 1;
-            echo "<li><a class='active_link' href='index.php?page={$pager_one}'>{$pager_one}</a></li>";
-            for ($i = 2; $i <= $count; $i++) {
-                if ($i == $page) {
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                } else {
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                }
-            }
-        } else {
-            for ($i = 1; $i <= $count; $i++) {
-                if (!isset($_GET['page'])) {
-                }
-                if ($i == $page) {
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                } else {
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                }
-            }
-        }
-
-
-
-        ?>
-    </ul>
-
-
     <!-- Footer -->
     <?php include "./includes/footer.php"; ?>
